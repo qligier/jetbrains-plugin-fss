ed80d9eeb70bee0ae5f0fef6919bc038ef36f282
Quentin Ligier <quentin.ligier@bluewin.ch>
2022-11-25 17:59:56 +0100

    Work on IgPublisherConfiguration

--- src/main/java/ch/qligier/jetbrains/plugin/fhir/configuration/IgPublisherSettingsEditor.java
+++ src/main/java/ch/qligier/jetbrains/plugin/fhir/configuration/IgPublisherSettingsEditor.java
@@ -8,6 +8,7 @@ import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.ui.TextBrowseFolderListener;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.Gray;
 import com.intellij.ui.JBColor;
 import org.jetbrains.annotations.NotNull;
 
@@ -21,18 +22,20 @@ import java.util.regex.Pattern;
  * The settings editor GUI for the IG Publisher configuration.
  *
  * @author Quentin Ligier
+ * @see <a href="https://jetbrains.design/intellij/principles/icons/#color">Color</a>
  **/
 public class IgPublisherSettingsEditor extends SettingsEditor<IgPublisherConfiguration> {
     public static final Logger LOG = Logger.getInstance("IgPublisherSettingsEditor");
     private static final Pattern IG_PUBLISHER_VERSION_PATTERN = Pattern.compile("\\d+\\.\\d+\\.\\d+");
-    private static final Color GREEN = new JBColor(new Color(77, 187, 79), new Color(77, 187, 79));
-    private static final Color RED = new JBColor(new Color(77, 187, 79), new Color(77, 187, 79));
+    private static final Color GREEN = new JBColor(new Color(89, 168, 105), new Color(89, 168, 105));
+    private static final Color RED = new JBColor(new Color(219, 88, 96), new Color(219, 88, 96));
+    private static final Color GRAY = new JBColor(Gray._133, Gray._133);
     private TextFieldWithBrowseButton textFieldJarPath;
     private JPanel globalPanel;
     private JLabel igPublisherStatus;
     private JCheckBox checkboxUseWatch;
     private JCheckBox checkboxUseTxServer;
-    private JTextField textFieldtxServerUrl;
+    private JTextField textFieldTxServerUrl;
     private JCheckBox checkBoxUseColorAndFancyChars;
 
     /**
@@ -43,7 +46,7 @@ public class IgPublisherSettingsEditor extends SettingsEditor<IgPublisherConfigu
         this.textFieldJarPath.setText(configuration.getOptions().getJarPath());
         this.checkboxUseWatch.setSelected(configuration.getOptions().getUseWatch());
         this.checkboxUseTxServer.setSelected(configuration.getOptions().getUseTxServer());
-        this.textFieldtxServerUrl.setText(configuration.getOptions().getTxServerUrl());
+        this.textFieldTxServerUrl.setText(configuration.getOptions().getTxServerUrl());
         this.checkBoxUseColorAndFancyChars.setSelected(configuration.getOptions().getUseColorAndFancyChars());
         this.updateTxServer();
         this.updateIgPublisherStatus();
@@ -57,7 +60,7 @@ public class IgPublisherSettingsEditor extends SettingsEditor<IgPublisherConfigu
         configuration.getOptions().setJarPath(this.textFieldJarPath.getText());
         configuration.getOptions().setUseWatch(this.checkboxUseWatch.isSelected());
         configuration.getOptions().setUseTxServer(this.checkboxUseTxServer.isSelected());
-        configuration.getOptions().setTxServerUrl(this.textFieldtxServerUrl.getText());
+        configuration.getOptions().setTxServerUrl(this.textFieldTxServerUrl.getText());
         configuration.getOptions().setUseColorAndFancyChars(this.checkBoxUseColorAndFancyChars.isSelected());
     }
 
@@ -70,11 +73,17 @@ public class IgPublisherSettingsEditor extends SettingsEditor<IgPublisherConfigu
     }
 
     protected void updateTxServer() {
-        this.textFieldtxServerUrl.setEnabled(this.checkboxUseTxServer.isSelected());
+        this.textFieldTxServerUrl.setEnabled(this.checkboxUseTxServer.isSelected());
     }
 
     protected void updateIgPublisherStatus() {
         final String jarPath = this.textFieldJarPath.getText();
+        if (jarPath.isBlank()) {
+            System.out.println("Case1");
+            this.igPublisherStatus.setText("An absolute path, or relative to the project root");
+            this.igPublisherStatus.setForeground(GRAY);
+            return;
+        }
         final var processBuilder = new ProcessBuilder();
         processBuilder.command("java", "-Dfile.encoding=UTF-8", "-jar", jarPath, "-v");
         try {
@@ -91,13 +100,14 @@ public class IgPublisherSettingsEditor extends SettingsEditor<IgPublisherConfigu
             if (exitVal == 0) {
                 System.out.println("[" + output + "]");
                 if (IG_PUBLISHER_VERSION_PATTERN.matcher(output).matches()) {
-                    this.igPublisherStatus.setText("Found IG Publisher v. " + output);
+                    this.igPublisherStatus.setText("Found IG Publisher v" + output);
                     this.igPublisherStatus.setForeground(GREEN);
                     return;
                 }
             }
 
         } catch (final Exception e) {
+            System.out.println("Case2");
             LOG.debug("Caught exception while executing IG Publisher JAR", e);
         }
         this.igPublisherStatus.setText("This doesn't seem to be an IG Publisher JAR file");
