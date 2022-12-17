7baee265b4ee0237fecd60de2fe56b3cb0b44ffe
Quentin Ligier <quentin.ligier@bluewin.ch>
2022-12-17 10:39:24 +0100

    Add live templates to FSH

---
+++ src/main/java/ch/qligier/jetbrains/plugin/fhir/fsh/FshContext.java
@@ -0,0 +1,27 @@
+// Copyright 2022 Quentin Ligier. Use of this source code is governed by the MIT license.
+
+package ch.qligier.jetbrains.plugin.fhir.fsh;
+
+import com.intellij.codeInsight.template.TemplateActionContext;
+import com.intellij.codeInsight.template.TemplateContextType;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A TemplateContextType tells the IntelliJ Platform where the Live Template is applicable: FSH files.
+ *
+ * @author Quentin Ligier
+ * @see <a
+ * href="https://plugins.jetbrains.com/docs/intellij/template-support.html#implement-templatecontexttype">Implement
+ * TemplateContextType</a>
+ **/
+public class FshContext extends TemplateContextType {
+
+    protected FshContext() {
+        super("FSH", "FSH");
+    }
+
+    @Override
+    public boolean isInContext(@NotNull final TemplateActionContext templateActionContext) {
+        return templateActionContext.getFile().getName().endsWith(".fsh");
+    }
+}
