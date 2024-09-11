3b37d729e32d7038fc84a016320422247a84339b
Quentin Ligier <quentin.ligier@bluewin.ch>
2024-09-11 21:02:19 +0200

    Add TextMate bundle

---
+++ src/main/java/ch/qligier/jetbrains/plugin/fhir/fsh/FshTextMateBundleProvider.java
@@ -0,0 +1,13 @@
+// Copyright 2024 Quentin Ligier. Use of this source code is governed by the MIT license.
+
+package ch.qligier.jetbrains.plugin.fhir.fsh;
+
+import org.jetbrains.plugins.textmate.api.TextMateBundleProvider;
+
+/**
+ * jetbrains-plugin-fhir
+ *
+ * @author Quentin Ligier
+ **/
+public class FshTextMateBundleProvider extends TextMateBundleProvider {
+}
