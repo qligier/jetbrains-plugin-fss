5260eab7778a3c6a9e733bdd1311f5b66882f04e
Quentin Ligier <quentin.ligier@bluewin.ch>
2025-12-22 07:44:39 +0100

    Some old work

--- src/main/kotlin/ch/qligier/jetbrains/plugin/fss/fsh/antlr/FshParserAdapter.kt
+++ src/main/kotlin/ch/qligier/jetbrains/plugin/fss/fsh/antlr/FshParserAdapter.kt
@@ -8,13 +8,17 @@ import ch.qligier.jetbrains.plugin.fss.fsh.FshLanguage
 import com.intellij.lang.ASTNode
 import com.intellij.lang.PsiBuilder
 import com.intellij.psi.impl.source.tree.FileElement
+import com.intellij.psi.impl.source.tree.LeafElement
+import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.psi.tree.IElementType
 import com.intellij.psi.tree.IFileElementType
+import org.antlr.intellij.adaptor.lexer.TokenIElementType
 import org.antlr.intellij.adaptor.parser.ANTLRParserAdaptor
 import org.antlr.v4.runtime.Parser
 import org.antlr.v4.runtime.tree.ParseTree
 
 class FshParserAdapter : ANTLRParserAdaptor(FshLanguage, FSHParser(null)) {
+
     override fun parse(
         parser: Parser,
         root: IElementType,
@@ -37,9 +41,53 @@ class FshParserAdapter : ANTLRParserAdaptor(FshLanguage, FSHParser(null)) {
         val node = super.parse(root, builder)
 
         if (node is FileElement) {
-            // todo: work on the AST here?
+            walkAstNode(node)
         }
 
         return node
+    }
+
+    private fun walkAstNode(node: ASTNode) {
+        if (node is LeafElement) {
+            updateLeaf(node)
+        }
+
+        var child = node.firstChildNode
+        while (child != null) {
+            walkAstNode(child)
+            child = child.treeNext
+        }
+    }
+
+    private fun updateLeaf(leaf: LeafElement) {
+        val antlrElementType = leaf.elementType as? TokenIElementType
+        if (antlrElementType == null) {
+            return
+        }
+        when (antlrElementType.antlrTokenType) {
+            FSHLexer.STAR -> updateStarLeaf(leaf)
+        }
+    }
+
+    private fun updateStarLeaf(starLeaf: LeafElement) {
+        if (starLeaf.text.startsWith("*")) {
+            return
+        }
+        val whitespacesLength = starLeaf.text.indexOf("*")
+        if (whitespacesLength < 1) {
+            return
+        }
+
+        /*starLeaf.rawInsertBeforeMe(LeafPsiElement(
+            TokenIElementType(FSHLexer.WHITESPACE, "whitespace", FshLanguage),
+            starLeaf.text.substring(0, whitespacesLength)))*/
+        //starLeaf.replaceWithText(starLeaf.text.substring(whitespacesLength))
+        //starLeaf.replaceWithText(starLeaf.text)
+
+        starLeaf.treeParent.replaceChild(starLeaf, LeafPsiElement(
+                starLeaf.elementType,
+            starLeaf.text))
+
+        //starLeaf.treeParent.replaceChild(starLeaf, starLeaf)
     }
 }
