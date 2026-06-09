198cbca2fdb4b8be9fc3dc079c4c604c1e68fd00
Quentin Ligier <3299399+qligier@users.noreply.github.com>
2026-06-09 20:34:28 +0200

    chore: rename plugin (#25)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased] - 2026-MM-DD
+
+### Changed
+
+- Rename the plugin to "FSH Toolkit", to respect both the FHIR® Trademark and JetBrains' guidelines against using the
+  word "support" in the plugin name.
+
 ## [0.0.7] - 2026-04-01
 
 ### Added
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@
  */
 
 import org.jetbrains.changelog.Changelog
-import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
 
 plugins {
     id("java") // Java support
@@ -59,23 +59,8 @@ dependencies {
 // Configure IntelliJ Platform Gradle Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-extension.html
 intellijPlatform {
     pluginConfiguration {
-        name = "FHIR and SUSHI Support"
         id = "jetbrains-plugin-fss"
         version = providers.gradleProperty("pluginVersion")
-
-        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-        description =
-            providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
-                val start = "<!-- Plugin description -->"
-                val end = "<!-- Plugin description end -->"
-
-                with(it.lines()) {
-                    if (!containsAll(listOf(start, end))) {
-                        throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-                    }
-                    subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
-                }
-            }
 
         val changelog = project.changelog // local variable for configuration cache compatibility
         // Get the latest available change notes from the changelog file
